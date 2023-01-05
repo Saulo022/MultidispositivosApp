60134e3429dae94b70f41a4323c0a2c72d6a0284
Saulo022 <saulo.nuez102@alu.ulpgc.es>
2023-01-05 20:14:22 +0000

    Initial commit

---
+++ app/src/main/java/com/example/multidispositivosapp/MoviesScreen.kt
@@ -0,0 +1,9 @@
+package com.example.multidispositivosapp
+
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun MoviesScreen(){
+
+}
---
+++ app/src/main/java/com/example/multidispositivosapp/model/Movie.kt
@@ -0,0 +1,3 @@
+package com.example.multidispositivosapp.model
+
+data class Movie()
